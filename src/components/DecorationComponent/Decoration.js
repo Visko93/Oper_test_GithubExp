10bd6912958cb0f037aedd9ba1d56213f49eb451
visko93 <vskonicezny@gmail.com>
2020-10-29 22:52:58 -0300

    first commit

---
+++ src/components/DecorationComponent/Decoration.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Decoration(props) {
+  return <h1>Decoration</h1>;
+}
+
+export default Decoration;
---
+++ src/components/SearhComponent/SearchComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SearchComponent(props) {
+  return (
+    <form onSubmit={() => {}}>
+      <header className="form__header">
+        <img src={} alt="" className="fomr__header-icon" />
+      </header>
+    </form>
+  );
+}
+
+export default SearchComponent;
